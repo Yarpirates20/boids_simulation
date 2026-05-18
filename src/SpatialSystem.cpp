2c7a0ea0d787bf7b1d4b6ff21aa7407ba2a155b8
Robert Samoraj <rsamoraj11@gmail.com>
2026-05-18 09:36:34 -0500

    Add SpatialSystem getNeighbors and helpers

---
+++ src/SpatialSystem.cpp
@@ -0,0 +1,53 @@
+#include "SpatialSystem.h"
+
+/** @copydoc SpatialSystem::getIndex(int x, int y) const */
+int SpatialSystem::getIndex(int x, int y) const
+{
+    return y * m_cols + x;
+}
+
+/** @copydoc SpatialSystem::getGridCoords(const sf::Vector2f &position) const */
+sf::Vector2i SpatialSystem::getGridCoords(const sf::Vector2f &position) const
+{
+    int x = static_cast<int>(position.x / m_cellSize);
+    int y = static_cast<int>(position.y / m_cellSize);
+
+    return sf::Vector2i(x, y);
+}
+
+/** @copydoc SpatialSystem::SpatialSystem(float width, float height, float cellSize) */
+SpatialSystem::SpatialSystem(float width, float height, float cellSize) : m_cellSize(cellSize), m_cols(static_cast<int>(width / cellSize) + 1), m_rows(static_cast<int>(height / cellSize) + 1), m_grid(m_cols * m_rows)
+{
+}
+
+/** @copydoc SpatialSystem::update(std::vector<std::shared_ptr<SimObject>> &objects, float dt) */
+void SpatialSystem::update(std::vector<std::shared_ptr<SimObject>> &objects, float dt)
+{
+}
+
+/** @copydoc patialSystem::getNeighbors(const sf::Vector2f &pos) const */
+std::vector<std::shared_ptr<SimObject>> SpatialSystem::getNeighbors(const sf::Vector2f &pos) const
+{
+    std::vector<std::shared_ptr<SimObject>> localNeighbors;
+
+    auto coord = getGridCoords(pos);
+
+    for (int dx = -1; dx <= 1; ++dx)
+    {
+        for (int dy = -1; dy <= 1; ++dy)
+        {
+            int neighborX = coord.x + dx;
+            int neighborY = coord.y + dy;
+
+            if (neighborX >= 0 && neighborX < m_cols && neighborY >= 0 && neighborY < m_rows)
+            {
+                int index = getIndex(neighborX, neighborY);
+
+                const auto &cellBoids = m_grid[index];
+                localNeighbors.insert(localNeighbors.end(), cellBoids.begin(), cellBoids.end());
+            }
+        }
+    }
+
+    return localNeighbors;
+}
--- src/include/SpatialSystem.h
+++ src/include/SpatialSystem.h
@@ -4,29 +4,49 @@
 #include "ISystem.h"
 #include "Boid.h"
 
-
 #include <vector>
 #include <memory>
 
-
-
 /**
  * @brief This class manages the boids in a grid cell and it's neighbors.
- * 
+ *
  */
 class SpatialSystem : public ISystem
 {
-    private:
-        float m_cellSize;
-        int m_cols;
-        int m_rows;
+private:
+    float m_cellSize;
+    int m_cols;
+    int m_rows;
 
-        std::vector<std::vector<std::shared_ptr<Boid>>> m_grid;
+    std::vector<std::vector<std::shared_ptr<Boid>>> m_grid;
 
-        int getIndex(int x, int y) const;
+    /**
+     * @brief Takes 2D grid coordinates and squashes them into 1D index.
+     * 
+     * @param x Grid X-coordinate.
+     * @param y Grid Y-coordinate.
+     * @return int The converted vector index.
+     */
+    int getIndex(int x, int y) const;
 
-    public:
-        SpatialSystem(float width, float height, float cellSize);
+    /**
+     * @brief Translates continuous world position (pixels) into discrete 2D 
+     * grid coordinates (columns and row columns).
+     * 
+     * @param position The pixel coordinates.
+     * @return sf::Vector2i The Grid coordinates.
+     */
+    sf::Vector2i getGridCoords(const sf::Vector2f &position) const;
+
+public:
+    /**
+     * @brief Construct a new Spatial System object.
+     * 
+     * @param width World width.
+     * @param height World height.
+     * @param cellSize Cell size approximate to perception radius.
+     */
+    SpatialSystem(float width, float height, float cellSize);
 
     void update(std::vector<std::shared_ptr<SimObject>> &objects, float dt) override;
 
